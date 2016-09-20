304dc6bff83492b504950c1dc4786547da7b6fc7
Marc Hibbins <marchibbins@gmail.com>
2016-09-20 14:05:13 +0100

    Add placeholder links to navigate

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class App extends React.Component {
 
@@ -6,6 +7,19 @@ export default class App extends React.Component {
         return (
             <main>
                 <h1>Home standard</h1>
+                <nav>
+                    <Link to='/'>Home</Link>
+                        <br/>
+                    <Link to='/affordability'>Affordability</Link>
+                        <br/>
+                    <Link to='/decency'>Decency</Link>
+                        <br/>
+                    <Link to='/neighbourhood'>Neighbourhood</Link>
+                        <br/>
+                    <Link to='/space'>Space</Link>
+                        <br/>
+                    <Link to='/stability'>Stability</Link>
+                </nav>
                 {this.props.children}
             </main>
         );
