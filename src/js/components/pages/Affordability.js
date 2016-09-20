09b70749c0af42e6d8f8ccdb61ae975006419163
Marc Hibbins <marchibbins@gmail.com>
2016-09-20 16:41:31 +0100

    Initial progress bars

--- src/js/components/pages/Affordability.js
+++ src/js/components/pages/Affordability.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion, Panel } from 'react-bootstrap';
+import { Accordion, Panel, ProgressBar } from 'react-bootstrap';
 
 export default class Affordability extends React.Component {
 
@@ -9,7 +9,24 @@ export default class Affordability extends React.Component {
                 <h2>Affordability</h2>
                 <Accordion defaultActiveKey={1}>
                     <Panel header="Income" eventKey={1}>
-                        Lorem ipsum
+                        <ul className="list-unstyled">
+                            <li>
+                                <h4>Value A</h4>
+                                <ProgressBar now={60} label={'60%'} srOnly/>
+                            </li>
+                            <li>
+                                <h4>Value B</h4>
+                                <ProgressBar now={70} label={"70%"} srOnly/>
+                            </li>
+                            <li>
+                                <h4>Value C</h4>
+                                <ProgressBar now={80} label={"80%"} srOnly/>
+                            </li>
+                            <li>
+                                <h4>Value D</h4>
+                                <ProgressBar now={65} label={"65%"} srOnly/>
+                            </li>
+                        </ul>
                     </Panel>
                     <Panel header="Tenure" eventKey={2}>
                         Lorem ipsum
