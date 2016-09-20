f46f70e899a00b0b72119b257b062355e698909f
Marc Hibbins <marchibbins@gmail.com>
2016-09-20 15:16:51 +0100

    Add navbar using react-bootstrap

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router';
+
+import Header from './Header';
 
 export default class App extends React.Component {
 
     render () {
         return (
-            <main>
-                <h1>Home standard</h1>
-                <nav>
-                    <Link to='/'>Home</Link>
-                        <br/>
-                    <Link to='/affordability'>Affordability</Link>
-                        <br/>
-                    <Link to='/decency'>Decency</Link>
-                        <br/>
-                    <Link to='/neighbourhood'>Neighbourhood</Link>
-                        <br/>
-                    <Link to='/space'>Space</Link>
-                        <br/>
-                    <Link to='/stability'>Stability</Link>
-                </nav>
-                {this.props.children}
-            </main>
+            <div>
+                <Header/>
+                <main>
+                    <h1>Home standard</h1>
+                    {this.props.children}
+                </main>
+            </div>
         );
     }
 
---
+++ src/js/components/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+export default class Header extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            expanded: false
+        };
+    }
+
+    handleNavbar (expanded) {
+        this.setState({expanded: expanded});
+    }
+
+    closeNavbar () {
+        this.setState({expanded: false});
+    }
+
+    render () {
+        return (
+            <Navbar expanded={this.state.expanded} onToggle={this.handleNavbar.bind(this)} inverse>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">Home standard</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to="/affordability">
+                            <NavItem onClick={this.closeNavbar.bind(this)}>
+                                Affordability</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/decency">
+                            <NavItem onClick={this.closeNavbar.bind(this)}>
+                                Decency</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/neighbourhood">
+                            <NavItem onClick={this.closeNavbar.bind(this)}>
+                                Neighbourhood</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/space">
+                            <NavItem onClick={this.closeNavbar.bind(this)}>
+                                Space</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/stability">
+                            <NavItem onClick={this.closeNavbar.bind(this)}>
+                                Stability</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
+
+}
