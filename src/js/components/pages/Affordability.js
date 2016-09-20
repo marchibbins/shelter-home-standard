7514f3dce81e01b5096a19e177fc220dcc25132a
Marc Hibbins <marchibbins@gmail.com>
2016-09-20 16:18:50 +0100

    Lorem ipsm accordion

--- src/js/components/pages/Affordability.js
+++ src/js/components/pages/Affordability.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Accordion, Panel } from 'react-bootstrap';
 
 export default class Affordability extends React.Component {
 
     render () {
         return (
-            <h2>Affordability</h2>
+            <article>
+                <h2>Affordability</h2>
+                <Accordion defaultActiveKey={1}>
+                    <Panel header="Income" eventKey={1}>
+                        Lorem ipsum
+                    </Panel>
+                    <Panel header="Tenure" eventKey={2}>
+                        Lorem ipsum
+                    </Panel>
+                    <Panel header="Household" eventKey={3}>
+                        Lorem ipsum
+                    </Panel>
+                </Accordion>
+            </article>
         );
     }
 
