d61bf0fe2526b8f2c8ef69c0924c890e92a92201
Marc Hibbins <marchibbins@gmail.com>
2016-10-05 16:15:18 +0100

    Use uppercase class

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -7,50 +7,50 @@ export default class Footer extends React.Component {
     render () {
         return (
             <footer className="footer full-width">
-                <Nav bsStyle="pills" stacked activeKey={1}>
+                <Nav bsStyle="pills" stacked>
                     <LinkContainer to="/">
-                        <NavItem className="navitem--primary">The Living Home Standard</NavItem>
+                        <NavItem className="navitem--primary text-uppercase">The Living Home Standard</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/great-home-debate">
-                        <NavItem className="navitem--secondary">The Great Home Debate</NavItem>
+                        <NavItem className="navitem--secondary text-uppercase">The Great Home Debate</NavItem>
                     </LinkContainer>
 
                     <LinkContainer to="/affordability">
-                        <NavItem className="navitem--large">Affordability</NavItem>
+                        <NavItem className="navitem--large text-uppercase">Affordability</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/security">
-                        <NavItem className="navitem--large">Security</NavItem>
+                        <NavItem className="navitem--large text-uppercase">Security</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/stability">
-                        <NavItem className="navitem--large">Stability</NavItem>
+                        <NavItem className="navitem--large text-uppercase">Stability</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/decency">
-                        <NavItem className="navitem--large">Decenct conditions</NavItem>
+                        <NavItem className="navitem--large text-uppercase">Decenct conditions</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/neighbourhood">
-                        <NavItem className="navitem--large">Neighbourhood</NavItem>
+                        <NavItem className="navitem--large text-uppercase">Neighbourhood</NavItem>
                     </LinkContainer>
 
                     <LinkContainer to="/">
-                        <NavItem>Executive summary</NavItem>
+                        <NavItem className="text-uppercase">Executive summary</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/">
-                        <NavItem>Foreword by Campbell Ross</NavItem>
+                        <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">Campbell Ross</span></NavItem>
                     </LinkContainer>
                     <LinkContainer to="/">
-                        <NavItem>Foreword by British Gas</NavItem>
+                        <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">British Gas</span></NavItem>
                     </LinkContainer>
                     <LinkContainer to="/">
-                        <NavItem>Foreword by IPSOS MORI</NavItem>
+                        <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">IPSOS MORI</span></NavItem>
                     </LinkContainer>
                     <LinkContainer to="/">
-                        <NavItem>Shelter and British Gas</NavItem>
+                        <NavItem className="text-uppercase">Shelter and British Gas</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/">
-                        <NavItem>Contributors</NavItem>
+                        <NavItem className="text-uppercase">Contributors</NavItem>
                     </LinkContainer>
                     <LinkContainer to="/">
-                        <NavItem>Contact</NavItem>
+                        <NavItem className="text-uppercase">Contact</NavItem>
                     </LinkContainer>
                 </Nav>
             </footer>
