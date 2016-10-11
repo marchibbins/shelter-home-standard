a1ece189924d09580cd8e3e26c16cc67597e3ab8
Marc Hibbins <marchibbins@gmail.com>
2016-10-11 16:12:40 +0100

    Columns for footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Nav, NavItem } from 'react-bootstrap';
+import { Col, Grid, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default class Footer extends React.Component {
@@ -9,52 +9,57 @@ export default class Footer extends React.Component {
             <footer>
                 <div className="footer">
                     <Grid>
-                        <Nav bsStyle="pills" stacked>
-                            <LinkContainer to="/">
-                                <NavItem className="navitem--primary text-uppercase">The Living Home Standard</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/great-home-debate">
-                                <NavItem className="navitem--secondary text-uppercase">The Great Home Debate</NavItem>
-                            </LinkContainer>
+                        <Col md={6}>
+                            <Nav bsStyle="pills" stacked>
+                                <LinkContainer to="/">
+                                    <NavItem className="navitem--primary text-uppercase">The Living Home Standard</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/great-home-debate">
+                                    <NavItem className="navitem--secondary text-uppercase">The Great Home Debate</NavItem>
+                                </LinkContainer>
 
-                            <LinkContainer to="/affordability">
-                                <NavItem className="navitem--large text-uppercase">Affordability</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/security">
-                                <NavItem className="navitem--large text-uppercase">Security</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/stability">
-                                <NavItem className="navitem--large text-uppercase">Stability</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/decent">
-                                <NavItem className="navitem--large text-uppercase">Decent conditions</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/neighbourhood">
-                                <NavItem className="navitem--large text-uppercase">Neighbourhood</NavItem>
-                            </LinkContainer>
-
-                            <LinkContainer to="/executive-summary">
-                                <NavItem className="text-uppercase">Executive summary</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/foreword-campbell-ross">
-                                <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">Campbell Ross</span></NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/foreword-british-gas">
-                                <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">British Gas</span></NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/foreword-ipsos-mori">
-                                <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">IPSOS MORI</span></NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/shelter-british-gas">
-                                <NavItem className="text-uppercase">Shelter and British Gas</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/contributors">
-                                <NavItem className="text-uppercase">Contributors</NavItem>
-                            </LinkContainer>
-                            <LinkContainer to="/contact">
-                                <NavItem className="text-uppercase">Contact</NavItem>
-                            </LinkContainer>
-                        </Nav>
+                                <LinkContainer to="/affordability">
+                                    <NavItem className="navitem--large text-uppercase">Affordability</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/security">
+                                    <NavItem className="navitem--large text-uppercase">Security</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/stability">
+                                    <NavItem className="navitem--large text-uppercase">Stability</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/decent">
+                                    <NavItem className="navitem--large text-uppercase">Decent conditions</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/neighbourhood">
+                                    <NavItem className="navitem--large text-uppercase">Neighbourhood</NavItem>
+                                </LinkContainer>
+                            </Nav>
+                        </Col>
+                        <Col md={6}>
+                            <Nav bsStyle="pills" stacked>
+                                <LinkContainer to="/executive-summary">
+                                    <NavItem className="text-uppercase">Executive summary</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/foreword-campbell-ross">
+                                    <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">Campbell Ross</span></NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/foreword-british-gas">
+                                    <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">British Gas</span></NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/foreword-ipsos-mori">
+                                    <NavItem><span className="text-uppercase">Foreword</span> by <span className="text-uppercase">IPSOS MORI</span></NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/shelter-british-gas">
+                                    <NavItem className="text-uppercase">Shelter and British Gas</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/contributors">
+                                    <NavItem className="text-uppercase">Contributors</NavItem>
+                                </LinkContainer>
+                                <LinkContainer to="/contact">
+                                    <NavItem className="text-uppercase">Contact</NavItem>
+                                </LinkContainer>
+                            </Nav>
+                        </Col>
                     </Grid>
                 </div>
                 <div className="logo-footer">
