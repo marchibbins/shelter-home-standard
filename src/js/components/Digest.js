d8cb6087c52f91ff80df9b2b1f1a41e182e2158d
Marc Hibbins <marchibbins@gmail.com>
2016-10-13 13:14:13 +0100

    Digest copy

--- src/js/components/Digest.js
+++ src/js/components/Digest.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { Col, Collapse, Nav, NavItem, Row, Tab } from 'react-bootstrap';
 
 export default class Digest extends React.Component {
@@ -40,14 +41,10 @@ export default class Digest extends React.Component {
                             <Tab.Pane eventKey="1" className="dimension-digest__panel">
                                 <Collapse in={this.state[1]}>
                                     <div>
-                                        <p className="lead">Shelter lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                        <p><a href="#" className="read-more">Read more</a></p>
+                                        <p className="lead">&ldquo;Developed by the public for the public, this ground-breaking piece of work amplifies Shelter’s vision of a home for everyone&rdquo;</p>
+                                        <p>We wanted to mark our 50th anniversary by reconnecting society with a united vision that we need to strive for – something more than a roof over our heads. Because while a roof is a necessity, it’s far from enough.</p>
+                                        <p>With the Living Wage as inspiration, we wanted something that would raise the bar for homes in this country. We’ve been able to take a holistic look at home, and provide an ambitious but realistic definition of what the British public believe everyone needs from a home in order to live rather than just get by.</p>
+                                        <p><Link to="/foreword-campbell-ross" className="read-more">Read more</Link></p>
                                     </div>
                                 </Collapse>
                             </Tab.Pane>
@@ -56,14 +53,11 @@ export default class Digest extends React.Component {
                             <Tab.Pane eventKey="2" className="dimension-digest__panel">
                                 <Collapse in={this.state[2]}>
                                     <div>
-                                        <p className="lead">British Gas ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                        <p><a href="#" className="read-more">Read more</a></p>
+                                        <p className="lead">&ldquo;We share a belief with Shelter that we should all have a safe, decent and warm place to call home&rdquo;</p>
+                                        <p>Working together for four years, we’ve raised over £1million and helped to improve living standards for thousands of families.</p>
+                                        <p>Home is central to our work as it’s vital to our customers. We need to do more to improve the standard of homes for everyone. We saw the development of the Living Home Standard as a powerful opportunity to do just this.</p>
+                                        <p>As well as building on our partnership with Shelter, we’ve been able to learn more from our customers about what’s important to them in a home and together produce something that has the potential to help us all have a safe, decent and warm place to call home.</p>
+                                        <p><Link to="/foreword-british-gas" className="read-more">Read more</Link></p>
                                     </div>
                                 </Collapse>
                             </Tab.Pane>
@@ -72,14 +66,11 @@ export default class Digest extends React.Component {
                             <Tab.Pane eventKey="3" className="dimension-digest__panel">
                                 <Collapse in={this.state[3]}>
                                     <div>
-                                        <p className="lead">Ipsos MORI ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                                        <p><a href="#" className="read-more">Read more</a></p>
+                                        <p className="lead">&ldquo;We studied existing standards, literature and evidence, and consulted with experts. However, first and foremost creating the Living Home Standard was about the public.&rdquo;</p>
+                                        <p>Defining a home that provides more than the bare minimum, that allows people to flourish, meant bringing in expertise from right across Ipsos MORI. Initial research into different aspects of housing uncovered four key themes: Affordability, Decent conditions, Space and Stability.</p>
+                                        <p>These themes – along with an additional theme of Neighbourhood which arose from the conversations we had with the public – were explored in discussion groups, workshops an online discussion community and quantitative research, to establish a list of 39 attributes the public believes are needed from a home to support an acceptable standard of physical, mental and social wellbeing.</p>
+                                        <p>Some attributes are classed as essentials – conditions that every home must meet in order to meet the Living Home Standard. Others are classed as tradables – features of a home which many people believed were important, but not universally applicable to or equally desired by everyone. A household can be measured against these 39 attributes to assess whether or not it meets the Living Home Standard.</p>
+                                        <p><Link to="/foreword-ipsos-mori" className="read-more">Read more</Link></p>
                                     </div>
                                 </Collapse>
                             </Tab.Pane>
