57de30d859a609eb2429e7324cedcc5c0018d750
Marc Hibbins <marchibbins@gmail.com>
2016-10-14 14:58:42 +0100

    Columnise forewords

--- src/js/components/pages/ForewordBritishGas.js
+++ src/js/components/pages/ForewordBritishGas.js
@@ -7,22 +7,19 @@ export default class ForewordBritishGas extends React.Component {
         return (
             <Grid>
                 <article>
-                    <h1>Foreword</h1>
-                    <h2>British Gas</h2>
-
-                    <p>At British Gas we share a belief with Shelter, that we should all have a safe, decent and warm place to call home. Working together for four years, we’ve raised over &pound;1million and helped to improve living standards for thousands of families.</p>
-
-                    <p>Our focus has been on improving standards in the private rented sector and we’re proud of all we have achieved together through sharing our expertise and knowledge.</p>
-
-                    <ul>
-                        <li>We’ve successfully campaigned to improve electrical and carbon monoxide safety standards in England, Scotland and Wales.</li>
-                        <li>And we’ve worked with Shelter in England and Wales to protect renters from retaliatory eviction &mdash; when landlords evict tenants for drawing attention to the problems in their property.</li>
-                        <li>Funding from the British Gas Energy Trust has also enabled training on fuel poverty and energy debt for all Shelter’s advisers as well as funding for fifteen specialist Shelter advisers who can provide tailored, free and impartial advice on how to stay out of debt.</li>
-                    </ul>
-
-                    <p>Home is central to our work as it’s vital to our customers. Our engineers make 9 million visits to their homes every year, whether that is to install insulation or smart meters, repair boilers, or conduct gas and electrical safety checks.  So we know that having a warm home is really important to them. We know from our work around the Connected Home that technology is transforming homes and the way people live. We also know that there are too many places in Britain where people are not happy to call home. So we need to do more to improve the standard of homes for everyone. </p>
-
-                    <p>We saw the development of the Living Home Standard as a powerful opportunity to do just this. As well as building on our partnership with Shelter, we’ve been able to learn more from our customers about what’s important to them in a home and together produce something that has the potential to help us all have a safe, decent and warm place to call home. </p>
+                    <Col md={7}>
+                        <h1>Foreword</h1>
+                        <h2>British Gas</h2>
+                        <p>At British Gas we share a belief with Shelter, that we should all have a safe, decent and warm place to call home. Working together for four years, we’ve raised over &pound;1million and helped to improve living standards for thousands of families.</p>
+                        <p>Our focus has been on improving standards in the private rented sector and we’re proud of all we have achieved together through sharing our expertise and knowledge.</p>
+                        <ul>
+                            <li>We’ve successfully campaigned to improve electrical and carbon monoxide safety standards in England, Scotland and Wales.</li>
+                            <li>And we’ve worked with Shelter in England and Wales to protect renters from retaliatory eviction &mdash; when landlords evict tenants for drawing attention to the problems in their property.</li>
+                            <li>Funding from the British Gas Energy Trust has also enabled training on fuel poverty and energy debt for all Shelter’s advisers as well as funding for fifteen specialist Shelter advisers who can provide tailored, free and impartial advice on how to stay out of debt.</li>
+                        </ul>
+                        <p>Home is central to our work as it’s vital to our customers. Our engineers make 9 million visits to their homes every year, whether that is to install insulation or smart meters, repair boilers, or conduct gas and electrical safety checks.  So we know that having a warm home is really important to them. We know from our work around the Connected Home that technology is transforming homes and the way people live. We also know that there are too many places in Britain where people are not happy to call home. So we need to do more to improve the standard of homes for everyone. </p>
+                        <p>We saw the development of the Living Home Standard as a powerful opportunity to do just this. As well as building on our partnership with Shelter, we’ve been able to learn more from our customers about what’s important to them in a home and together produce something that has the potential to help us all have a safe, decent and warm place to call home. </p>
+                   </Col>
                 </article>
             </Grid>
         );
