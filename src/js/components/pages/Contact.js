f7674a8aabfdc77cfc613830092b67b7d5e54422
Marc Hibbins <marchibbins@gmail.com>
2016-10-17 11:21:05 +0100

    Update contacts page

--- src/js/components/pages/Contact.js
+++ src/js/components/pages/Contact.js
@@ -14,10 +14,7 @@ export default class Copy extends React.Component {
                                 <p><img src="/livinghomestandard/assets/img/contact-campbell-robb.jpg" className="img-responsive"/></p>
                                 <h3>Campbell Robb</h3>
                                 <h4>Chief Executive</h4>
-                                <p>
-                                    Tel: 0344 515 1182<br/>
-                                    Email: <a href="mailto:public_affairs@shelter.org.uk">public_affairs@shelter.org.uk</a>
-                                </p>
+                                <p><a href="mailto:public_affairs@shelter.org.uk">Email</a></p>
                             </div>
                         </Col>
                         <Col sm={6} md={3}>
@@ -25,10 +22,7 @@ export default class Copy extends React.Component {
                                 <p><img src="/livinghomestandard/assets/img/contact-roger-harding.jpg" className="img-responsive"/></p>
                                 <h3>Roger Harding</h3>
                                 <h4>Director of Communications, Policy and Campaigns</h4>
-                                <p>
-                                    Tel: 0344 515 1182<br/>
-                                    Email: <a href="mailto:public_affairs@shelter.org.uk">public_affairs@shelter.org.uk</a>
-                                </p>
+                                <p><a href="mailto:public_affairs@shelter.org.uk">Email</a></p>
                             </div>
                         </Col>
                         <Col sm={6} md={3}>
@@ -36,10 +30,7 @@ export default class Copy extends React.Component {
                                 <p><img src="/livinghomestandard/assets/img/contact-hilary-burkitt.jpg" className="img-responsive"/></p>
                                 <h3>Hilary Burkitt</h3>
                                 <h4>Head of Research</h4>
-                                <p>
-                                    Tel: 0344 515 1182<br/>
-                                    Email: <a href="mailto:public_affairs@shelter.org.uk">public_affairs@shelter.org.uk</a>
-                                </p>
+                                <p><a href="mailto:public_affairs@shelter.org.uk">Email</a></p>
                             </div>
                         </Col>
                         <Col sm={6} md={3}>
@@ -47,13 +38,11 @@ export default class Copy extends React.Component {
                                 <p><img src="/livinghomestandard/assets/img/contact-steve-akehurst.jpg" className="img-responsive"/></p>
                                 <h3>Steve Akehurst</h3>
                                 <h4>Head of Public Affairs</h4>
-                                <p>
-                                    Tel: 0344 515 1182<br/>
-                                    Email: <a href="mailto:public_affairs@shelter.org.uk">public_affairs@shelter.org.uk</a>
-                                </p>
+                                <p><a href="mailto:public_affairs@shelter.org.uk">Email</a></p>
                             </div>
                         </Col>
                     </Row>
+                    <p>Telephone: 0344 515 1182</p>
                 </article>
             </Grid>
         );
