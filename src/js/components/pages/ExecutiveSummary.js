1a03a0f009374a5e84c58ced9544fbb7c3379757
Marc Hibbins <marchibbins@gmail.com>
2016-10-17 16:21:46 +0100

    Copy lists

--- src/js/components/pages/ExecutiveSummary.js
+++ src/js/components/pages/ExecutiveSummary.js
@@ -16,13 +16,15 @@ export default class ExecutiveSummary extends React.Component {
 
                             <p>The 39 statements which make up the Living Home Standard are split across five different dimensions:</p>
 
-                            <ul>
-                                <li>Decent conditions</li>
-                                <li>Affordability</li>
-                                <li>Space</li>
-                                <li>Stability</li>
-                                <li>Neighbourhood</li>
-                            </ul>
+                            <p>
+                                <ul>
+                                    <li>Decent conditions</li>
+                                    <li>Affordability</li>
+                                    <li>Space</li>
+                                    <li>Stability</li>
+                                    <li>Neighbourhood</li>
+                                </ul>
+                            </p>
 
                             <p>Within each dimension some attributes are classed as essentials – conditions that every home must meet in order to meet the Living Home Standard. Other attributes were classed as tradables, features many people believed were important, but they were not universally applicable to or equally desired by everyone. A home needs to meet a certain number of the tradable conditions in each dimension of the Standard, but not all, in order for it to achieve the Living Home Standard overall.</p>
 
@@ -32,44 +34,50 @@ export default class ExecutiveSummary extends React.Component {
 
                             <h3>Affordability</h3>
 
-                            <ul>
-                                <li>Can meet the rent or mortgage payments on the home without regularly having to cut spending on household essentials like food or heating</li>
-                                <li>Not worried that rent or mortgage payments could rise to a level that would be difficult to pay</li>
-                                <li>Can meet rent or mortgage payments on the home without regularly preventing participation in social activities</li>
-                                <li>Can meet the rent or mortgage payments on the home without regularly being prevented from putting enough money aside to cover unexpected costs</li>
-                            </ul>
+                            <p>
+                                <ul>
+                                    <li>Can meet the rent or mortgage payments on the home without regularly having to cut spending on household essentials like food or heating</li>
+                                    <li>Not worried that rent or mortgage payments could rise to a level that would be difficult to pay</li>
+                                    <li>Can meet rent or mortgage payments on the home without regularly preventing participation in social activities</li>
+                                    <li>Can meet the rent or mortgage payments on the home without regularly being prevented from putting enough money aside to cover unexpected costs</li>
+                                </ul>
+                            </p>
 
                             <h3>Decent conditions</h3>
 
-                            <ul>
-                                <li>The home can be heated safely and effectively</li>
-                                <li>The home has hot and cold running water</li>
-                                <li>The home is free from safety hazards such as faulty wiring or fire risks</li>
-                                <li>The home is structurally sound with no important defects to the roof and/or walls</li>
-                                <li>The home has a toilet, and a bath and/or shower</li>
-                                <li>The home feels physically secure (for example with adequate locks on doors and windows)</li>
-                                <li>The home is free from pest problems</li>
-                                <li>The home is free from mould or damp problems</li>
-                                <li>The home is suitable for the current age and/or disability related needs of everyone in the household</li>
-                                <li>There is adequate space to store essential items, such as a reasonable amount of clothes, towels and bedding</li>
-                                <li>There are enough bathroom facilities for everyone living in the household to be able to use them at a time suitable for their needs</li>
-                                <li>The home has access to outdoor space, for example a private or shared garden, or balcony</li>
-                                <li>There is enough space to have visitors during the day or evening</li>
-                                <li>There is enough space for members of the household to study or do work or homework if they need to</li>
-                                <li>There is enough space for a guest to stay overnight, for instance on a sofa or sofa bed</li>
-                                <li>There is enough space to store domestic items like vacuum cleaners and ironing boards to keep them out of the way</li>
-                                <li>There is enough space to store some personal belongings, such as books or children’s toys</li>
-                            </ul>
+                            <p>
+                                <ul>
+                                    <li>The home can be heated safely and effectively</li>
+                                    <li>The home has hot and cold running water</li>
+                                    <li>The home is free from safety hazards such as faulty wiring or fire risks</li>
+                                    <li>The home is structurally sound with no important defects to the roof and/or walls</li>
+                                    <li>The home has a toilet, and a bath and/or shower</li>
+                                    <li>The home feels physically secure (for example with adequate locks on doors and windows)</li>
+                                    <li>The home is free from pest problems</li>
+                                    <li>The home is free from mould or damp problems</li>
+                                    <li>The home is suitable for the current age and/or disability related needs of everyone in the household</li>
+                                    <li>There is adequate space to store essential items, such as a reasonable amount of clothes, towels and bedding</li>
+                                    <li>There are enough bathroom facilities for everyone living in the household to be able to use them at a time suitable for their needs</li>
+                                    <li>The home has access to outdoor space, for example a private or shared garden, or balcony</li>
+                                    <li>There is enough space to have visitors during the day or evening</li>
+                                    <li>There is enough space for members of the household to study or do work or homework if they need to</li>
+                                    <li>There is enough space for a guest to stay overnight, for instance on a sofa or sofa bed</li>
+                                    <li>There is enough space to store domestic items like vacuum cleaners and ironing boards to keep them out of the way</li>
+                                    <li>There is enough space to store some personal belongings, such as books or children’s toys</li>
+                                </ul>
+                            </p>
 
                             <h3>Neighbourhood</h3>
 
-                            <ul>
-                                <li>Feel reasonably safe and secure in the local neighbourhood</li>
-                                <li>Amenities such as grocery shops, schools and/or a doctor’s surgery, are within reasonable reach of the home</li>
-                                <li>The home is close enough to family, friends or other support networks</li>
-                                <li>Anyone in the household who works outside the home can usually reach their place of work in an hour or less</li>
-                           </ul>
-                        </Col>
+                            <p>
+                                <ul>
+                                    <li>Feel reasonably safe and secure in the local neighbourhood</li>
+                                    <li>Amenities such as grocery shops, schools and/or a doctor’s surgery, are within reasonable reach of the home</li>
+                                    <li>The home is close enough to family, friends or other support networks</li>
+                                    <li>Anyone in the household who works outside the home can usually reach their place of work in an hour or less</li>
+                               </ul>
+                            </p>
+                            </Col>
                     </Row>
                 </article>
            </Grid>
--- src/js/components/pages/ForewordBritishGas.js
+++ src/js/components/pages/ForewordBritishGas.js
@@ -18,11 +18,13 @@ export default class ForewordBritishGas extends React.Component {
                         <Col md={7} mdPull={5}>
                             <p>At British Gas we share a belief with Shelter, that everyone should have a safe, decent and warm place to call home. Working together for four years, we’ve raised over £1million and helped to improve living standards for thousands of families.</p>
                             <p>Our shared focus has been on improving standards in the private rented sector. We’re proud of all we have achieved together by pooling our expertise and knowledge.</p>
-                            <ul>
-                                <li>We’ve successfully campaigned to improve electrical and carbon monoxide safety standards in England, Scotland and Wales.</li>
-                                <li>And we’ve worked with Shelter in England and Wales to protect renters from retaliatory eviction - when landlords evict tenants for drawing attention to the problems in their property.</li>
-                                <li>Funding from the British Gas Energy Trust has also enabled training on fuel poverty and energy debt for all Shelter’s advisers as well as funding for fifteen Shelter specialists who can provide tailored, free and impartial advice on how to stay out of debt.</li>
-                            </ul>
+                            <p>
+                                <ul>
+                                    <li>We’ve successfully campaigned to improve electrical and carbon monoxide safety standards in England, Scotland and Wales.</li>
+                                    <li>And we’ve worked with Shelter in England and Wales to protect renters from retaliatory eviction - when landlords evict tenants for drawing attention to the problems in their property.</li>
+                                    <li>Funding from the British Gas Energy Trust has also enabled training on fuel poverty and energy debt for all Shelter’s advisers as well as funding for fifteen Shelter specialists who can provide tailored, free and impartial advice on how to stay out of debt.</li>
+                                </ul>
+                            </p>
                             <p>Home is central to everything we do, because it’s vital to our customers. Our engineers make 9 million visits to their homes every year, whether that is to install insulation, repair boilers or conduct gas and electrical safety checks. They’re also working hard to bring homes in to the 21st century with smart meters and remote-controlled heating.</p>
                             <p>We know from our Connected Home business that technology is transforming homes and the way people live. But we also know that in too many parts of Britain people are not happy to call where they live “home”. So we need to do more to improve the standard of homes for everyone.</p>
                             <p>We saw the development of the Living Home Standard as a powerful opportunity to do just this. We’ve been able to learn more from our customers about what’s important to them in a home, and with Shelter have produced something that has the potential to help everyone have a safe, decent and warm place to call home.</p>
